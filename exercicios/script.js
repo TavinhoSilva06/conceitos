b757eb242d4cf74dcab456cbad6cffeb12422038
TavinhoSilva06 <tavinho.augusto.amaral@gmail.com>
2024-09-17 08:53:02 -0300

    Criando um CRUD

---
+++ exercicios/script.js
@@ -0,0 +1,25 @@
+let form = document.getElementById("form");
+let input = document.getElementById("input");
+let msgErro = document.getElementById("msgErro");
+let posts = document.getElementById("posts");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault(); 
+    validarFormulario();
+});
+
+let validarFormulario = () => {
+    if (input.value === "") {
+        msgErro.innerHTML = "Preencha o campo!";
+        input.focus();
+    } else {
+        msgErro.innerHTML = ""; // Limpando a mensagem de erro
+        aceitarDados();
+    }
+};
+
+let aceitarDados = () => {
+    let dados = {};
+    dados["Valor de Entrada"] = input.value;
+    console.log(dados);
+};
